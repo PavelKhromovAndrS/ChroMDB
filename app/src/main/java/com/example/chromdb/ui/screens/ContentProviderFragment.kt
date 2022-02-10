2b8a928b82683924b95f1a07563593e7cc0d0808
PavelKhromovAndrS <pavelkhromov1993@yandex.ru>
2022-02-10 22:44:10 +0300

    Added a screen with Google maps

--- app/src/main/java/com/example/chromdb/ui/screens/ContentProviderFragment.kt
+++ app/src/main/java/com/example/chromdb/ui/screens/ContentProviderFragment.kt
@@ -2,20 +2,18 @@ package com.example.chromdb.ui.screens
 
 import android.Manifest
 import android.annotation.SuppressLint
-import android.app.AlertDialog
 import android.content.ContentResolver
 import android.content.Context
 import android.content.pm.PackageManager
 import android.database.Cursor
 import android.os.Bundle
 import android.provider.ContactsContract
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.appcompat.widget.AppCompatTextView
 import androidx.core.content.ContextCompat
-import com.example.chromdb.R
+import androidx.fragment.app.Fragment
 import com.example.chromdb.databinding.FragmentContentProviderBinding
 
 const val REQUEST_CODE = 42
